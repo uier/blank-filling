5d13342899963b51a52484c656e9d3fb3419f288
TzuWei <ap9940506@gmail.com>
2021-11-11 19:18:37 +0800

    remove outdated code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import VueHotkey from 'v-hotkey'
 
-createApp(App).use(VueHotkey).mount('#app')
+createApp(App).mount('#app')
